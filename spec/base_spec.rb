20f4f3f95312ea357c4afafbd1af4509d6fac1a1
Jason <jason@pemulis>
2015-11-03 16:47:33 -0600

    Fix the Sinatra version to avoid an issue with exiting immediately b/c of source file path

--- spec/base_spec.rb
+++ spec/base_spec.rb
@@ -6,13 +6,16 @@ describe ExampleWebSocketApp do
 	it { should respond_to(:run!) }
 	it { should respond_to(:socket_send) }
 	it { should respond_to(:socket_close) }
-	its(:websocket_config) { should be_an_instance_of(Hash) }
 
 	context "override Base defaults" do
 		let(:example) {
 			ExampleWebSocketApp.new(http_port: 80, http_host: "localhost")
 		}
 		let(:config) { example.websocket_config }
+
+		it "has a config hash" do
+			config.should be_an_instance_of(Hash)
+		end
 
 		it "has defaults inherited from Base" do
 			config[:socket_port].should equal(8080)
